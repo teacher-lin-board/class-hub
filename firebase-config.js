8e30cbc041065ff392b19efb08651a2710408e52
teacher-lin-board <t00428@hyes.tyc.edu.tw>
2026-07-26 23:24:20 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+﻿// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAT9VXm8gf5Pn4A1iu3xXF6X8Ve9NYlUAk",
+  authDomain: "teacherboard-9224d.firebaseapp.com",
+  projectId: "teacherboard-9224d",
+  storageBucket: "teacherboard-9224d.firebasestorage.app",
+  messagingSenderId: "801036213120",
+  appId: "1:801036213120:web:2077de68b0a74395023f65"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
